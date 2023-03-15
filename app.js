f4b8e640740b12ffa77bce281362998d154badeb
brrrrrbrrrr <benjamin.chaillan-pro@laposte.net>
2023-03-15 17:11:44 +0100

    challenge done

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ app.post('/api/movies', validateMovie, movieHandlers.postMovie);
 app.post('/api/users', validateUser, userHandlers.postCreateUser);
 app.put('/api/movies/:id', validateMovie, movieHandlers.putMovie);
 app.put('/api/users/:id', validateUser, userHandlers.putUser);
+app.delete('/api/movies/:id', movieHandlers.deleteMovie);
+app.delete('/api/users/:id', userHandlers.deleteUser);
 
 app.listen(port, (err) => {
   if (err) {
